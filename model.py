75ced0e9245d2f2ce55bccac2bd1c8510f6805b8
zwh42 <zhaowenhao@outlook.com>
2017-03-04 08:16:17 +0000

    1st working model

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ from keras.layers.core import Lambda, Dense, Activation, Flatten
 import matplotlib.pyplot as plt
 from collections import Counter
 
-TRAIN_DATA_PATH = [r"../P3DATA/train_data_0"]  #[r"../P3DATA/train_data_0",r"../P3DATA/train_data_3",r"../P3DATA/train_data_4", r"../P3DATA/train_data_5", r"../P3DATA/train_data_6"]
+TRAIN_DATA_PATH = [r"../P3DATA/train_data_1",r"../P3DATA/train_data_0",r"../P3DATA/train_data_3",r"../P3DATA/train_data_4", r"../P3DATA/train_data_5", r"../P3DATA/train_data_6"]
 
 DO_VISUALIZE = False
 
@@ -69,7 +69,7 @@ def data_preprocessing(csv_path_list):
     #do sample cleaning 
     for sample in sample_line_list:
         if abs(float(sample[3]) - 0.00) < 0.001: #to many angle == 0, only chose part
-            if random.random() <= most_common_angles[1][1]/most_common_angles[0][1] * 2:
+            if random.random() <= most_common_angles[1][1]/most_common_angles[0][1] * 4:
                 final_sample_list.append(sample)
                 clean_counter[sample[3]] += 1
         else:
@@ -106,7 +106,7 @@ def generator(samples, batch_size=1000):
 
             images = []
             angles = []
-            angle_correction = [0,+0.25, -0.25] #steering angle correction for center, left, right angles
+            angle_correction = [0,+0.3, -0.3] #steering angle correction for center, left, right angles
             for batch_sample in batch_samples:
                 #handle center, left, right images
                 for i in range(3):    
@@ -205,7 +205,7 @@ def model_setup():
     #lambda layer: to normalize images to [-0.5, +0.5] 
     model.add(Lambda(lambda x: x/255.-0.5)) 
     
-    model.add(Convolution2D(64, 3, 3))
+    model.add(Convolution2D(3, 3, 3))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.5))
     model.add(ELU())
@@ -223,12 +223,40 @@ def model_setup():
     model.add(Dropout(0.4))
     model.add(Dense(32))
 
-    model.add(Dense(1, activation='elu'))
+    model.add(Dense(1))
     
     return model
 
-def mode_setup2():
+def model_setup2():
     model = Sequential()
+    #model = Sequential()
+    # Preprocess incoming data, centered around zero with small standard deviation 
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
+    model.add(Convolution2D(24,5,5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(36,5,5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(48,3,3))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64,3,3))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(1164))
+    model.add(Activation('relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+    model.add(Dense(1))  
     return model
 
 
@@ -237,7 +265,7 @@ def flow_setup():
     
     samples = data_preprocessing(TRAIN_DATA_PATH)
     print("total sample count:", len(samples))
-    train_validation_samples, test_samples = train_test_split(samples, test_size = 0.2, random_state = 42)
+    train_validation_samples, test_samples = train_test_split(samples, test_size = 0.1, random_state = 42)
     train_samples, validation_samples = train_test_split(train_validation_samples, test_size = 0.2, random_state = 42)
     print("train sample count: ", len(train_samples), "\nvalidation sample count: ", len(validation_samples), "\ntest sample count: ", len(test_samples))
     print("sample data example:\n", train_samples[random.randint(0, len(train_samples))])
@@ -247,12 +275,12 @@ def flow_setup():
     test_generator = generator(test_samples, batch_size = 32)            
     #print("generator output example: \n", next(train_generator))
     
-    model = model_setup()
+    model = model_setup2()
     model.compile(loss = "mse", optimizer="adam")
     print(model.summary())
     
     
-    history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3, verbose=1)
+    history_object = model.fit_generator(train_generator, samples_per_epoch= int(len(train_samples)/2), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=10, verbose=1)
     score = model.evaluate_generator(test_generator, 1500, max_q_size=10, nb_worker=1, pickle_safe=False)
     print(score)
     
